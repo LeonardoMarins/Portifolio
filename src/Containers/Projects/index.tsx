8d72572ad4d3c523902f74fd039ccda8ad159543
Leonardo <lupadinho867@gmail.com>
2023-09-12 14:03:11 -0300

    texto

--- src/Containers/Projects/index.tsx
+++ src/Containers/Projects/index.tsx
@@ -27,8 +27,8 @@ const Projects = () => {
       <h1 className="titulo">Projetos</h1>
       <Avatar src={avatar} alt="" />
       <Card background="blue" descricao='Desenvolvendo um sistema de formulários,checklist e controle de equipamentos da empresa OEG offshore, em conjunto com a empresa junior case.' nome='Sistema oeg' image={oeg} tecnologia1='SQL' tecnologia2='PHP' tecnologia3='JAVASCRIPT' />
-      <Card href="https://eplay-gray.vercel.app/" button background="black" descricao='Desenvolvimento de um site de jogos do curso da EBAC onde tivemos que consumir uma API e montar o layout com um modal e um carrinho de compras utilizando o redux para gerenciamento de estado.' nome='Eplay' image={eplay} tecnologia1='REACT' tecnologia2='STYLED-COMPONENTS' tecnologia3='REDUX' />
-      <Card href="https://contabilidadefortes.com.br/" button background="" descricao='Criação do site da Fortes contabilidade pela empresa junior Case da faculdade Femass onde atualmente sou o Diretor de projetos' nome='Site Fortes' image={fortes} tecnologia1='REACT' tecnologia2='STYLED-COMPONENTS' tecnologia3='REDUX' />
+      <Card href="https://eplay-gray.vercel.app/" button background="black" descricao='Desenvolvimento de um site de jogos do curso da EBAC, onde foi necessário consumir uma API e criar o layout, incluindo a implementação de um modal e um carrinho de compras, utilizando o Redux para o gerenciamento de estado.' nome='Eplay' image={eplay} tecnologia1='REACT' tecnologia2='STYLED-COMPONENTS' tecnologia3='REDUX' />
+      <Card href="https://contabilidadefortes.com.br/" button background="" descricao='Criação do site da Fortes Contabilidade pela empresa júnior Case da Faculdade Femass, onde atualmente exerço o cargo de Diretor de Projetos.' nome='Site Fortes' image={fortes} tecnologia1='REACT' tecnologia2='STYLED-COMPONENTS' tecnologia3='REDUX' />
       </Container>
       <a href="https://github.com/LeonardoMarins" target="_blank"><ButtonGithub>Visitar o Github<img src={github} alt="icone github" /></ButtonGithub></a>
       </ContainerDiv>
